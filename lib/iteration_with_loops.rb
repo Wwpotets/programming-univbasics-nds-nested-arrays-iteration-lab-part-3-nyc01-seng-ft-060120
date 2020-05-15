63a281794f881283c7cfa465bd354f3257d474d5
Corey Lynch <bearinawolfpack@gmail.com>
2020-05-15 19:33:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,17 @@
 def join_nested_strings(src)
   y_counter = 0
-  	target_data = []
+	target_data = []
 
-  	while y_counter < array.length do
-  		x_counter = 0
+	while y_counter < array.length do
+		x_counter = 0
 
-  		while x_counter < array[y_counter].length do
-  			if array[y_counter][x_counter].class == String
-  				target_data.push(array[y_counter][x_counter])
-  			end
-  			x_counter += 1
-  		end
-  		y_counter += 1
-  	end
-  	p target_data
+		while x_counter < array[y_counter].length do
+			if array[y_counter][x_counter].class == String
+				target_data.push(array[y_counter][x_counter])
+			end
+			x_counter += 1
+		end
+		y_counter += 1
+	end
+	p target_data.join(' ')
 end
